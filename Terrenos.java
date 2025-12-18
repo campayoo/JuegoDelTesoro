e6cad3cef4e31bc598f209347aac8055c11330f4
campayo <pcampayo07@gmail.com>
2025-12-18 19:59:50 +0100

    Update Terrenos.java

--- Terrenos.java
+++ Terrenos.java
@@ -1,7 +1,7 @@
 public class Terrenos {
     public static final int TAM = 10;
-    public static final char TIERRA = '.';
-    public static final char MAR = '~';
-    public static final char JUGADOR = 'P';
-    public static final char TESORO = 'X';
+    public static final char TIERRA = '䷀';
+    public static final char MAR = '♨';
+    public static final char JUGADOR = '☺';
+    public static final char TESORO = '✖';
 }
